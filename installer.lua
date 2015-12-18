5b35a76dbde7223072d443d616474d0f13a03585
acharluk <acharluk@gmail.com>
2015-12-18 19:41:32 +0100

    Uploaded MaccOS installer

--- installer.lua
+++ installer.lua
@@ -1,3 +1,10 @@
-files = {
+shell.run("rm *")
 
-}
+file = fs.open("grd","w")
+file.write(http.get("https://raw.githubusercontent.com/MaCCOS/Installer/master/grd").readAll())
+file.close()
+
+shell.run("grd MaCCOS MaCCOS / . .")
+shell.run("mv MaCCOS/* /")
+shell.run("rm README.md MaCCOS")
+os.reboot()
